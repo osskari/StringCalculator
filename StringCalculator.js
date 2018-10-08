ca0bebceeb627af719eb3d7539b5a603322b6358
osskari <oskarjorgen@gmail.com>
2018-10-08 19:40:12 +0000

    The Add function can now recieve numbers in seperate lines instead of just one.

--- StringCalculator.js
+++ StringCalculator.js
@@ -2,11 +2,13 @@ function Add(numbers) {
     if(numbers == "") {
         return 0;
     }
-    var a = numbers.split(',').map(Number);
+    var separators = [',', '\n'];
+    var a = numbers.split(new RegExp(separators.join('|'), 'g')).map(Number);
     var retval = 0;
     for(var i = 0; i < a.length; i++){
         retval += a[i];
     }
+    console.log(retval);
     return retval;
 }
 
