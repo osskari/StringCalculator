b522923f7545cb18bf6407fac7fb14700d9fdae7
osskari <oskarjorgen@gmail.com>
2018-10-23 23:01:59 +0000

    circleci init

--- Add.test.js
+++ Add.test.js
@@ -1,33 +1,33 @@
 const calculator = require('./StringCalculator')
 
-test("inputs an empty string into the Add function", () => {
+it("inputs an empty string into the Add function", () => {
     expect(calculator("")).toBe(0);
 });
 
-test("inputs one number into the Add function" , () => {
+it("inputs one number into the Add function" , () => {
     expect(calculator("2")).toBe(2);
 });
 
-test("inputs 2 numbers into the Add function", () => {
+it("inputs 2 numbers into the Add function", () => {
     expect(calculator("2,5")).toBe(7);
 });
 
-test("inputs 10 numbers into the Add function", () => {
+it("inputs 10 numbers into the Add function", () => {
     expect(calculator("1,2,3,4,5,6,7,8,9,10")).toBe(55);
 });
 
-test("inputs a negative number into the Add function", () => {
+it("inputs a negative number into the Add function", () => {
     expect(calculator("-1,-2,-3")).toBe();
 });
 
-test("inputs some numbers including some over 1000", () => {
+it("inputs some numbers including some over 1000", () => {
     expect(calculator("1,2,3,4,1001,5,6,1005,7,8,9,10")).toBe(55);
 });
 
-test("inputs a single character delimeter and some numbers", () => {
+it("inputs a single character delimeter and some numbers", () => {
     expect(calculator("//!/n1!2!3!4!5")).toBe(15);
 });
 
-test("inputs a stubstring as a delimiter and some numbers", () => {
+it("inputs a stubstring as a delimiter and some numbers", () => {
     expect(calculator("//delim/n1delim2delim3delim4delim5delim6")).toBe(21);
 });
